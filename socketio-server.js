0b032b2f59caed604449a8ffc24cdbc62f19a1cc
suxinren <suxinren@hotmail.com>
2020-06-24 11:04:55 +0800

    Update socketio-server.js

--- socketio-server.js
+++ socketio-server.js
@@ -13,19 +13,19 @@ io.on('connection', function(socket){
 	console.log('client connected');
 	console.log('client num:'+clients);
 	
-	//socket.emit('boop');//向建立该连接的客户端广播
-	//socket.broadcast.emit('boop');//向除了自己之外的其它客户端广播
-	//io.sockets.emit('boop') ;//向所有客户端广播，等于以上两个之和
+	//socket.emit('boop');
+	//socket.broadcast.emit('boop');
+	//io.sockets.emit('boop') ;
 	//socket.on('message', function(message, callback) {});
 	
-	socket.on('message', function(msg){  //接收socket连接消息的时候触发
+	socket.on('message', function(msg){
         console.log('received a message:'+msg.content);
-        socket.broadcast.emit('message',msg);//向除了消息发送者之外的其他所有客户端广播
+        socket.broadcast.emit('message',msg);
     });
     
-    socket.on('messageall', function(msg){  //接收socket连接消息的时候触发
+    socket.on('messageall', function(msg){
         console.log('received a message:'+msg.content);
-        io.sockets.emit('messageall',msg) ;//向所有客户端广播，包括消息发送者
+        io.sockets.emit('messageall',msg) ;
     });
     
 	socket.on('disconnect', function () {
